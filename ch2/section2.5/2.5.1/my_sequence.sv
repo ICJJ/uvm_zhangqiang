7c27f6b86d0cff960d4817566aee89806230b152
ICJJ <875327102@qq.com>
2024-08-22 12:56:09 +0800

    first commit

---
+++ ch2/section2.5/2.5.1/my_sequence.sv
@@ -0,0 +1,27 @@
+`ifndef MY_SEQUENCE__SV
+`define MY_SEQUENCE__SV
+
+class my_sequence extends uvm_sequence #(my_transaction);
+   my_transaction m_trans;
+
+   function new(string name= "my_sequence");
+      super.new(name);
+   endfunction
+
+   virtual task body();
+      `ifdef UVM_VERSION_1_2
+        starting_phase=get_starting_phase();
+     `endif
+      if(starting_phase != null) 
+         starting_phase.raise_objection(this);
+      repeat (10) begin
+         `uvm_do(m_trans)
+      end
+      #1000;
+      if(starting_phase != null) 
+         starting_phase.drop_objection(this);
+   endtask
+
+   `uvm_object_utils(my_sequence)
+endclass
+`endif
---
+++ ch3/section3.5/3.5.5/sameLayerDiffComponent/my_agent.sv
@@ -0,0 +1,45 @@
+`ifndef MY_AGENT__SV
+`define MY_AGENT__SV
+
+class my_agent extends uvm_agent ;
+   my_sequencer  sqr;
+   my_driver     drv;
+   my_monitor    mon;
+   
+   uvm_analysis_port #(my_transaction)  ap;
+   
+   function new(string name, uvm_component parent);
+      super.new(name, parent);
+   endfunction 
+   
+   extern virtual function void build_phase(uvm_phase phase);
+   extern virtual function void connect_phase(uvm_phase phase);
+
+   `uvm_component_utils(my_agent)
+endclass 
+
+
+function void my_agent::build_phase(uvm_phase phase);
+   super.build_phase(phase);
+   if (is_active == UVM_ACTIVE) begin
+      sqr = my_sequencer::type_id::create("sqr", this);
+      drv = my_driver::type_id::create("drv", this);
+   end
+   mon = my_monitor::type_id::create("mon", this);
+   uvm_config_db#(int)::set(this, 
+                            "drv", 
+                            "pre_num", 
+                            100);
+   `uvm_info("my_agent", "in my_agent, drv.pre_num is set to 200", UVM_LOW)
+endfunction 
+
+function void my_agent::connect_phase(uvm_phase phase);
+   super.connect_phase(phase);
+   if (is_active == UVM_ACTIVE) begin
+      drv.seq_item_port.connect(sqr.seq_item_export);
+   end
+   ap = mon.ap;
+endfunction
+
+`endif
+
